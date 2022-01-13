302da601281108abd35e964577cb1bf2f9a6efe6
katrina <katrina.nov13@gmail.com>
2022-01-13 16:34:34 +0300

    08. reducers, tests, sort

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,21 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType = {type: 'sort', payload: string} | {type: 'check', payload: number}
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return [...state].sort((a, b) => {
+                if (action.payload === 'up') {
+                    return a.name <= b.name ? -1 : 1
+                } else {
+                    return a.name <= b.name ? 1 : -1
+                }
+
+            });
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(f => f.age > action.payload)
         }
         default: return state
     }
